d061f73f0a30c6269719877adfd0d87cb16f9477
Umme-25 <ummehabiba2241@gmail.com>
2026-06-13 18:11:26 +0530

    Add

--- The_initializer.py
+++ The_initializer.py
@@ -7,10 +7,11 @@ class Student:
     This is a class Student to manage student ifo and activities
     """
 
-    def __init__(self,name,roll):
+    def __init__(self,name,roll,dept):
         print("Calling the initializer")
         self.name = name
         self.roll = roll
+        self.department = dept
 
     def study(self,n_hours):
         print(f"Self is:{self}")  #o/p => self is: <__main__.Student object at memory address
@@ -19,6 +20,16 @@ class Student:
     def sports(self,sports_name):
         print(f"The student plays {sports_name}")
     
-student1 = Student()
+student1 = Student("John",1001,"Science")
 
-student2 = Student()
+student2 = Student("Carol",1002,"Arts")
+
+print(student1.name,student1.roll,student1.department)
+print(student2.name,student2.roll,student2.department)
+
+print(student1.__dict__)
+print(student2.__dict__)
+
+"""
+Instance variable/attribute are different for different objects
+"""
